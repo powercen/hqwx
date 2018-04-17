a30d5e3f3731f95f27d930524f9f36a6029cd1ea
powercen <3565645@qq.com>
2018-04-17 22:36:42 +0800

    添加关注事件响应有错误，修复bug

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -10,18 +10,11 @@ use EasyWeChat\kernel\Messages\Text;
 class WeChatController extends Controller
 {
 
-    protected $app;
-
-    public function __construct()
-    {
-        $this->app = app('wechat.official_account');
-    }
-
     public function serve()
     {
         Log::info('request arrived.');
-
-        $this->app->server->push(function ($message){
+        $app = app('wechat.official_account');
+        $app->server->push(function ($message){
             switch ($message->MsgType){
                 case 'event':
                     //关注消息，新用户关注进入公众号
@@ -36,7 +29,7 @@ class WeChatController extends Controller
             return '这是回复消息';
         });
 
-        return $this->app->server->serve();
+        return $app->server->serve();
     }
 
 

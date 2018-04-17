5cb9f6663d70d83fd301b886e410ed6c0c5870ca
powercen <3565645@qq.com>
2018-04-17 22:18:32 +0800

    添加关注事件响应，推送第一条消息

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use EasyWeChat\kernel\Messages\Text;
 
 class WeChatController extends Controller
 {
@@ -20,11 +22,23 @@ class WeChatController extends Controller
         Log::info('request arrived.');
 
         $this->app->server->push(function ($message){
-            return '华庆作业区';
+            switch ($message->MsgType){
+                case 'event':
+                    //关注消息，新用户关注进入公众号
+                    if($message->Event === 'subscribe'){
+                        return new Text('你好，欢迎来到华庆作业区');
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+            return '这是回复消息';
         });
 
         return $this->app->server->serve();
     }
+
 
 
 }
